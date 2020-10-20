3cef29b0915ed8b274c5025b367947be7c916cdf
Szczepaniak-M <m.szczepaniak99@gmail.com>
2020-10-20 22:38:11 +0200

    Delete compiled files

--- target/classes/data.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO bookcrossing.tb_book (title, author, description)
-VALUES ('Czysty kod. Podręcznik dobrego programisty', 'Robert C. Martin', 'Bardzo ważna książka');
-
-INSERT INTO bookcrossing.tb_user (firstName, surname)
-VALUES ('Adam', 'Nowak');
