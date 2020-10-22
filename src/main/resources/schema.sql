13d1de506df9c7069545c87abafc379235f25f6a
Piotr Derenowski <56509461+piter104@users.noreply.github.com>
2020-10-22 09:59:00 +0200

    update

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,22 +1,29 @@
+CREATE TABLE IF NOT EXISTS tb_user(
+    id INT AUTO_INCREMENT NOT NULL,
+    first_name VARCHAR(255),
+    last_name VARCHAR(255),
+    PRIMARY KEY (id)
+);
+
 CREATE TABLE IF NOT EXISTS tb_book(
     id INT AUTO_INCREMENT NOT NULL ,
     title VARCHAR (255) NOT NULL,
     author VARCHAR (255) NOT NULL,
     description TEXT,
     owner_id INT,
-    PRIMARY KEY (id)
-);
-
-CREATE TABLE IF NOT EXISTS tb_user (
-     id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
-     firstName VARCHAR(255),
-     lastName VARCHAR(255)
+    current_owner_id INT,
+    PRIMARY KEY (id),
+    FOREIGN KEY (owner_id) REFERENCES tb_user(id),
+    FOREIGN KEY (current_owner_id) REFERENCES tb_user(id)
 );
 
 CREATE TABLE IF NOT EXISTS tb_opinion (
-     id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
+     id INT AUTO_INCREMENT NOT NULL,
      rating INT NOT NULL,
      description TEXT,
-     bookId INT NOT NULL,
-     authorId INT NOT NULL
+     book_id INT NOT NULL,
+     author_id INT NOT NULL,
+     PRIMARY KEY(id),
+     FOREIGN KEY(book_id) REFERENCES tb_book(id),
+     FOREIGN KEY(author_id) REFERENCES tb_user(id)
 );
