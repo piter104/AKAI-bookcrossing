5ee224c9297c2a76f0dad30c603b2ee7fec69f6b
Szczepaniak-M <m.szczepaniak99@gmail.com>
2020-10-26 20:17:38 +0100

    Add null check in index

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -4,5 +4,8 @@ VALUES ('Adam Nowak', 'adam.nowak@akai.org.pl');
 INSERT INTO bookcrossing.tb_book (title, author, description, owner_id, reader_id)
 VALUES ('Czysty kod. Podręcznik dobrego programisty', 'Robert C. Martin', 'Bardzo ważna książka', 1, 1);
 
+INSERT INTO bookcrossing.tb_book (title, author, description, owner_id, reader_id)
+VALUES ('Czysty kod. Podręcznik dobrego programisty', 'Robert C. Martin', 'Bardzo ważna książka', NULL, NULL);
+
 INSERT INTO bookcrossing.tb_opinion (rating, book_id, author_id)
 VALUES (6, 1, 1);
