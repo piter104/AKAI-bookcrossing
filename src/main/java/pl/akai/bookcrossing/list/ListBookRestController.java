130d012b3cfa643f64ebb8317b04881d72735199
piter104 <piotr.derenowski@gmail.com>
2020-10-26 20:00:32 +0100

    Book Add Form

--- src/main/java/pl/akai/bookcrossing/list/ListBookRestController.java
+++ src/main/java/pl/akai/bookcrossing/list/ListBookRestController.java
@@ -6,6 +6,9 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import pl.akai.bookcrossing.model.Book;
 
 @Controller
 public class ListBookRestController {
@@ -24,5 +27,18 @@ public class ListBookRestController {
         model.addAttribute("name", currentPrincipalName);
         model.addAttribute("books", bookDao.findAllBooks());
         return "index";
+    }
+
+    @GetMapping("/form")
+    public String greetingForm(Model model) {
+        model.addAttribute("book", new Book());
+        return "form";
+    }
+
+    @PostMapping("/form")
+    public String greetingSubmit(@ModelAttribute Book book, Model model) {
+        model.addAttribute("book", book);
+        bookDao.insertBook(book);
+        return "formResult";
     }
 }
